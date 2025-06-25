8969c9694977dca629afd31a415c44ad7c00054c
Jan Horstmann <horstmann@osism.tech>
2025-06-25 14:43:25 +0200

    Upload OSISM node image to glance
    Copy the node image into the osism-ansible container and upload it to
    glance.
    Default to the node image in the manager's `conductor.yml`.
    
    Signed-off-by: Jan Horstmann <horstmann@osism.tech>

--- scripts/ironic-import.sh
+++ scripts/ironic-import.sh
@@ -3,3 +3,4 @@
 docker exec osism-ansible mkdir -p /share/ironic/ironic
 docker cp /opt/ironic-agent.initramfs osism-ansible:/share/ironic/ironic/ironic-agent.initramfs
 docker cp /opt/ironic-agent.kernel osism-ansible:/share/ironic/ironic/ironic-agent.kernel
+docker cp /opt/osism.qcow2 osism-ansible:/share/ironic/ironic/osism.qcow2
