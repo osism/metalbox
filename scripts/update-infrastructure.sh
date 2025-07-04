cc045a2cf9dd2b7608092011188788781397e198
Christian Berendt <berendt@osism.tech>
2025-07-04 10:13:27 +0200

    Pull openstack and infrastruture images before the upgrade (#150)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/update-infrastructure.sh
+++ scripts/update-infrastructure.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+osism apply -a pull common
+osism apply -a pull redis
+osism apply -a pull memcached
+osism apply -a pull rabbitmq
+osism apply -a pull mariadb
+
 osism apply -a upgrade common
 osism apply -a upgrade redis
 osism apply -a upgrade memcached
